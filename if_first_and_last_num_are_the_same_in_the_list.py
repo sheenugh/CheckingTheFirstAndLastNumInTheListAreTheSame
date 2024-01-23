e15270f9c004ad11eccd8a23c355d1b499a64b58
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 07:30:00 +0800

    given x, it worked

--- if_first_and_last_num_are_the_same_in_the_list.py
+++ if_first_and_last_num_are_the_same_in_the_list.py
@@ -2,7 +2,8 @@
 
 #----- FUNCTIONS -----
 def is_last_and_first_num_same(numbers):
-    
+    if numbers:
+        return numbers[0] == numbers[4]
 
 # PSEUDO CODE
 # - Actual Code
@@ -10,7 +11,8 @@ def is_last_and_first_num_same(numbers):
 numbers_x = [10, 20, 30, 40, 10]
 numbers_y = [75, 65, 35, 75, 30]
 
-
+result_in_x = is_last_and_first_num_same(numbers_x)
+print("The result is: ", result_in_x)
 
 
 # - Result
