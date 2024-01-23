7436d0da70f40e149289e1b2172328a00779888c
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 07:27:56 +0800

    datas in pseudo code

---
+++ if_first_and_last_num_are_the_same_in_the_list.py
@@ -0,0 +1,16 @@
+
+
+#----- FUNCTIONS -----
+def is_last_and_first_num_same(numbers):
+    
+
+# PSEUDO CODE
+# - Actual Code
+# - Given lists
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
+
+
+
+
+# - Result
